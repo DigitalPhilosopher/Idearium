1e35e5b9dbe1b74788c828dca75ce451ac0c2ce2
Patrick Kühn <patrick@kuehntechlabs.de>
2024-07-03 18:39:04 +0200

    Initial Commit

---
+++ Idearium/IdeariumApp.swift
@@ -0,0 +1,32 @@
+//
+//  IdeariumApp.swift
+//  Idearium
+//
+//  Created by Patrick Kühn on 03.07.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IdeariumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
